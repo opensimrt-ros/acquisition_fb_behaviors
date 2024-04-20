31d1499599958f2b9980f8749285da08a8a68b25
frederico-klein <frekle@kth.se>
2024-04-20 14:08:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# acquisition_fb_behaviors
+This repo contains all acquisition_fb-specific states and behaviors.
---
+++ acquisition_fb_flexbe_states/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages = ['acquisition_fb_flexbe_states'],
+    package_dir = {'': 'src'}
+)
+
+setup(**d)
